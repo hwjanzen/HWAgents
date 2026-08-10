88cd63d2c724b4224f8e568e4c612c967f786bad
Alexander Janzen <ajanzen@hanfwolf.de>
2026-08-10 09:34:50 +0200

    fix: activate extension on startup

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import { registerCommands } from "./commands";
 
 export function activate(context: vscode.ExtensionContext): void {
+  console.log("HW Agents extension activated");
   registerCommands(context);
 }
 
